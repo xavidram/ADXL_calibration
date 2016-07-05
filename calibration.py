ee876549004409627cad2da1d13e54383227189a
Xavid Ramirez <xavid.ramirez01@utrgv.edu>
2016-07-05 10:46:28 -0500

    pusing test files

---
+++ calibration.py
@@ -0,0 +1,16 @@
+import os, sys, adxl345
+
+class xyz:
+	x = 0
+	y = 0
+	z = 0
+
+	def __init__(self,x,y,z)
+		self.x = x
+		self.y = y
+		self.z = z
+
+	def axis():
+		print "X:%d,Y:%d,Z:%d" % (x,y,z)
+
+
